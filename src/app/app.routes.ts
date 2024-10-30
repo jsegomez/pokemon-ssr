a03f3037b81dc185730d000edd67da21d423ff59
Josue Eliezer Gomez Soto <jsegomez06@gmail.com>
2024-10-29 20:16:47 -0600

    base project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'about',
+        loadComponent: () => import('./pages/about-page/about-page.component').then(c => c.AboutPageComponent)
+    },
+    {
+        path: 'pricing',
+        loadComponent: () => import('./pages/pricing-page/pricing-page.component').then(c => c.PricingPageComponent)
+    },
+    {
+        path: 'contact',
+        loadComponent: () => import('./pages/contact/contact.component').then(c => c.ContactComponent)
+    },
+    {
+        path: '**',
+        redirectTo: 'about'
+    }
+];
